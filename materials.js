12f33df716805cb64d19a1a495ebbbcb10329868
DungeonFox <93831751+DungeonFox@users.noreply.github.com>
2025-06-05 22:44:49 -0400

    Expand compute shader and material helpers

--- materials.js
+++ materials.js
@@ -8,3 +8,31 @@ export function createSubCubeMaterial() {
 export function createCubeMaterial(color) {
     return new t.MeshBasicMaterial({ color, wireframe: true });
 }
+
+export function createPointsMaterial(texture) {
+    return new t.ShaderMaterial({
+        uniforms: {
+            map: { value: texture },
+            size: { value: 1.0 },
+            opacity: { value: 1.0 }
+        },
+        vertexShader: `
+            attribute float alpha;
+            varying float vAlpha;
+            void main() {
+                vAlpha = alpha;
+                gl_PointSize = size;
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+            }
+        `,
+        fragmentShader: `
+            uniform sampler2D map;
+            varying float vAlpha;
+            void main() {
+                gl_FragColor = texture2D(map, gl_PointCoord) * vAlpha;
+            }
+        `,
+        transparent: true,
+        depthWrite: false
+    });
+}
